ed135b4e12bb3bd57ceeddb1d73988b4d136b2bc
fernandaaaaaaa <kadiazsa@est.utn.ac.cr>
2025-11-22 10:14:09 -0600

    feat: Add Water Quality Monitoring module (wat_kamila)

--- app/src/main/java/cr/ac/utn/movil/MainActivity.kt
+++ app/src/main/java/cr/ac/utn/movil/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.content.Intent
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -145,10 +145,11 @@ class MainActivity : AppCompatActivity() {
         btnProduction_main.setOnClickListener(View.OnClickListener{ view->
             //prod_
         })
-
-        val btnWater_main = findViewById<Button>(R.id.btnTemplate_main)
+        val btnWater_main = findViewById<Button>(R.id.btnWater_main)
         btnWater_main.setOnClickListener(View.OnClickListener{ view->
             //wat_
+            val intent = Intent(this, WatMainActivity::class.java)
+            startActivity(intent)
         })
 
         val btnRecycling_main = findViewById<Button>(R.id.btnTemplate_main)
